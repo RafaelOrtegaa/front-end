cba00b532916876aff1fb6500fe3bf411bda24de
RafaOrtegaa <rafaelortegacardoso@gmail.com>
2026-04-13 16:34:32 -0300

    Revisando elementos javascript

---
+++ revisão/objeto.js
@@ -0,0 +1,29 @@
+const pessoa = {
+    nome: "João",
+    sobrenome: "Roccella",
+    idade: 40, 
+    cargo: "Instrutor"
+};
+
+const entidade = new Map([
+    ["nome", "João"],
+    ["sobrenome", "Roccella"],
+    ["idade", 40],
+    ["cargo", "Instrutor"]
+]);
+
+console.log(entidade.get("sobrenome"));
+console.log(pessoa.sobrenome);
+
+const professor = JSON.stringify(pessoa);
+console.log(professor);
+
+const aluno = '{"nome":"Bento", "sobrenome":"Andrade", "idade":69, "matricula":123456}'
+const alunoObjeto = JSON.parse(aluno);
+console.log(alunoObjeto);
+console.log(aluno.nome);
+console.log(aluno.nome);
+
+for (let x in pessoa){
+    console.log(pessoa[x])
+}
